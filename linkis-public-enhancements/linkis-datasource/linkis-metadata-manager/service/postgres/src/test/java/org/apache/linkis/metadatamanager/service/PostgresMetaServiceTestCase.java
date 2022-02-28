452c63bcc63dbcaf202cc495d9666f7a1d85b1b1
nuts.jian <35603505+nutsjian@users.noreply.github.com>
2022-02-28 19:06:48 +0800

    feat: greenplum db2 sqlserver db2

---
+++ linkis-public-enhancements/linkis-datasource/linkis-metadata-manager/service/postgres/src/test/java/org/apache/linkis/metadatamanager/service/PostgresMetaServiceTestCase.java
@@ -0,0 +1,58 @@
+package org.apache.linkis.metadatamanager.service;
+
+import org.apache.linkis.metadatamanager.common.domain.MetaColumnInfo;
+import org.apache.linkis.metadatamanager.common.service.MetadataService;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PostgresMetaServiceTestCase {
+
+    private Map<String, Object> noDbParams;
+    private Map<String, Object> params;
+
+    @Before
+    public void before() {
+        params = new HashMap<>();
+        params.put("host", "localhost");
+        params.put("port", 15432);
+        params.put("username", "postgres");
+        params.put("password", "123456");
+        params.put("database", "gujian");
+
+        noDbParams = new HashMap<>();
+        noDbParams.put("host", "localhost");
+        noDbParams.put("port", 15432);
+        noDbParams.put("username", "postgres");
+        noDbParams.put("password", "123456");
+    }
+
+    @Test
+    public void testGetDatabases() {
+        MetadataService metadataService = new PostgresMetaService();
+        List<String> databases = metadataService.getDatabases("", noDbParams);
+        System.out.println(databases);
+    }
+
+    @Test
+    public void testGetTables() {
+        MetadataService metadataService = new PostgresMetaService();
+        // 这里的 public 参数是 pg 中的 schema
+        List<String> tables = metadataService.getTables("", params, "public");
+        System.out.println(tables);
+    }
+
+    @Test
+    public void testGetColumns() {
+        MetadataService metadataService = new PostgresMetaService();
+        List<MetaColumnInfo> columns = metadataService.getColumns("", params, "public", "gujian_table1");
+
+        for (MetaColumnInfo column : columns) {
+            System.out.println(column.getIndex() + "\t" + column.getName() + "\t" + column.getType());
+        }
+    }
+
+}
---
+++ linkis-public-enhancements/linkis-datasource/linkis-metadata-manager/service/sqlserver/src/test/java/org/apache/linkis/metadatamanager/service/SqlConnectionTc.java
@@ -0,0 +1,34 @@
+package org.apache.linkis.metadatamanager.service;
+
+import org.apache.linkis.metadatamanager.common.domain.MetaColumnInfo;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+
+public class SqlConnectionTc {
+
+    @Test
+    public void test01() throws SQLException, ClassNotFoundException {
+        SqlConnection connection = new SqlConnection("192.168.0.71", 1433, "sa", "123456asd@", new HashMap<>());
+        List<String> allDatabases = connection.getAllDatabases();
+        System.out.println(allDatabases);
+    }
+
+    @Test
+    public void test02() throws SQLException, ClassNotFoundException {
+        SqlConnection connection = new SqlConnection("192.168.0.71", 1433, "sa", "123456asd@", new HashMap<>());
+        List<String> allTables = connection.getAllTables("ReportServer");
+        System.out.println(allTables);
+    }
+
+    @Test
+    public void test03() throws SQLException, ClassNotFoundException {
+        SqlConnection connection = new SqlConnection("192.168.0.71", 1433, "sa", "123456asd@", new HashMap<>());
+        List<MetaColumnInfo> allColumns = connection.getColumns("ReportServer", "Users");
+        for (MetaColumnInfo column : allColumns) {
+            System.out.println(column.getIndex() + "\t" + column.getName() + "\t" + column.getType());
+        }
+    }
+}
