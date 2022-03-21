75172c28744a23b5e798d3f152ba6c521f453dea
limeng <limeng@shineweng.com>
2022-03-21 17:03:34 +0800

    datax

--- linkis-computation-governance/linkis-client/linkis-computation-client/src/test/java/org/apache/linkis/computation/client/DataXOnceJobTest2.java
+++ linkis-computation-governance/linkis-client/linkis-computation-client/src/test/java/org/apache/linkis/computation/client/DataXOnceJobTest2.java
@@ -10,82 +10,57 @@ import java.util.Map;
 
 public class DataXOnceJobTest2 {
     public static void main(String[] args) {
-        LinkisJobClient.config().setDefaultServerUrl("http://124.70.31.149:20088");
+        LinkisJobClient.config().setDefaultServerUrl("http://192.168.0.25:8088");
         /**
          */
         String code = "{\n" +
-                "\t\"content\":[\n" +
-                "\t\t{\n" +
-                "\t\t\t\"reader\":{\n" +
-                "\t\t\t\t\"parameter\":{\n" +
-                "\t\t\t\t\t\"password\":\"scm_@casc2f\",\n" +
-                "\t\t\t\t\t\"column\":[\n" +
-                "\t\t\t\t\t\t{\n" +
-                "\t\t\t\t\t\t\t\"name\":\"id\",\n" +
-                "\t\t\t\t\t\t\t\"type\":\"INT\"\n" +
-                "\t\t\t\t\t\t},\n" +
-                "\t\t\t\t\t\t{\n" +
-                "\t\t\t\t\t\t\t\"name\":\"age\",\n" +
-                "\t\t\t\t\t\t\t\"type\":\"INT\"\n" +
-                "\t\t\t\t\t\t}\n" +
-                "\t\t\t\t\t],\n" +
-                "\t\t\t\t\t\"connection\":[\n" +
-                "\t\t\t\t\t\t{\n" +
-                "\t\t\t\t\t\t\t\"jdbcUrl\":[\n" +
-                "\t\t\t\t\t\t\t\t{\n" +
-                "\t\t\t\t\t\t\t\t\t\"database\":\"test\",\n" +
-                "\t\t\t\t\t\t\t\t\t\"port\":\"3306\",\n" +
-                "\t\t\t\t\t\t\t\t\t\"host\":\"192.168.0.66\"\n" +
-                "\t\t\t\t\t\t\t\t}\n" +
-                "\t\t\t\t\t\t\t],\n" +
-                "\t\t\t\t\t\t\t\"table\":[\n" +
-                "\t\t\t\t\t\t\t\t\"t_psn\"\n" +
-                "\t\t\t\t\t\t\t]\n" +
-                "\t\t\t\t\t\t}\n" +
-                "\t\t\t\t\t],\n" +
-                "\t\t\t\t\t\"username\":\"scm\"\n" +
-                "\t\t\t\t},\n" +
-                "\t\t\t\t\"name\":\"mysqlreader\"\n" +
-                "\t\t\t},\n" +
-                "\t\t\t\"writer\":{\n" +
-                "\t\t\t\t\"parameter\":{\n" +
-                "\t\t\t\t\t\"path\":\"/user/hive/warehouse/psn\",\n" +
-                "\t\t\t\t\t\"fileName\":\"psn\",\n" +
-                "\t\t\t\t\t\"hiveMetastoreUris\":\"thrift://192.168.0.111:9083\",\n" +
-                "\t\t\t\t\t\"column\":[\n" +
-                "\t\t\t\t\t\t{\n" +
-                "\t\t\t\t\t\t\t\"index\":0,\n" +
-                "\t\t\t\t\t\t\t\"type\":\"int\"\n" +
-                "\t\t\t\t\t\t},\n" +
-                "\t\t\t\t\t\t{\n" +
-                "\t\t\t\t\t\t\t\"index\":1,\n" +
-                "\t\t\t\t\t\t\t\"type\":\"int\"\n" +
-                "\t\t\t\t\t\t}\n" +
-                "\t\t\t\t\t],\n" +
-                "\t\t\t\t\t\"defaultFS\":\"hdfs://ecs-f0cf-0001:1429\",\n" +
-                "\t\t\t\t\t\"writeMode\":\"truncate\",\n" +
-                "\t\t\t\t\t\"fieldDelimiter\":\",\",\n" +
-                "\t\t\t\t\t\"fileType\":\"text\"\n" +
-                "\t\t\t\t},\n" +
-                "\t\t\t\t\"name\":\"hdfswriter\"\n" +
-                "\t\t\t}\n" +
-                "\t\t}\n" +
-                "\t],\n" +
-                "\t\"setting\":{\n" +
-                "\t\t\"syncMeta\":\"true\",\n" +
-                "\t\t\"errorLimit\":{\n" +
-                "\t\t\t\"record\":\"100\"\n" +
-                "\t\t},\n" +
-                "\t\t\"useProcessor\":\"false\",\n" +
-                "\t\t\"speed\":{\n" +
-                "\t\t\t\"byte\":\"10240\",\n" +
-                "\t\t\t\"record\":\"1000\",\n" +
-                "\t\t\t\"channel\":\"1\"\n" +
-                "\t\t},\n" +
-                "\t\t\"advance\":{\n" +
-                "\t\t\t\"mMemory\":\"1000m\"\n" +
-                "\t\t}\n" +
-                "\t}\n" +
+                "    \"job\": {\n" +
+                "        \"content\":[\n" +
+                "            {\n" +
+                "                \"reader\": {\n" +
+                "                    \"name\": \"txtfilereader\", \n" +
+                "                    \"parameter\": {\n" +
+                "                        \"path\":[\"/opt/install/datax/data/test1.csv\"],\n" +
+                "                        \"encoding\":\"gbk\",\n" +
+                "                        \"column\": [\n" +
+                "                            {\n" +
+                "                                \"index\":0,\n" +
+                "                                \"type\":\"string\"\n" +
+                "                            },\n" +
+                "                            {\n" +
+                "                                \"index\":1,\n" +
+                "                                \"type\":\"string\"\n" +
+                "                            }\n" +
+                "                        ], \n" +
+                "                        \"fileldDelimiter\":\",\"\n" +
+                "                    }\n" +
+                "                }, \n" +
+                "                \"writer\": {\n" +
+                "                    \"name\": \"mysqlwriter\", \n" +
+                "                    \"parameter\": {\n" +
+                "                        \"username\": \"scm\",\n" +
+                "                        \"password\": \"scm_@casc2f\", \n" +
+                "                        \"column\": [\n" +
+                "                            \"i\",\n" +
+                "                            \"j\"\n" +
+                "                        ],\n" +
+                "                        \"preSql\": [], \n" +
+                "                        \"connection\": [\n" +
+                "                            {\n" +
+                "                                \"jdbcUrl\":\"jdbc:mysql://192.168.0.21:3306/test\", \n" +
+                "                                \"table\": [\"testtab\"]\n" +
+                "                            }\n" +
+                "                        ]\n" +
+                "                    }\n" +
+                "                }\n" +
+                "            }\n" +
+                "        ], \n" +
+                "        \"setting\": {\n" +
+                "            \"speed\": {\n" +
+                "                \"channel\": \"4\"\n" +
+                "            }\n" +
+                "        }\n" +
+                "    }\n" +
                 "}";
 
         Map<String,String> rwMaps = new HashMap<>();
